11f67fabe36a36738b899b74b18e49ebe2b60894
Karsten Bjelde <fotball-karsten@hotmail.com>
2020-09-16 19:10:56 +0200

    I dont know if this is correct, but I did it

---
+++ js/components/displayHtml.js
@@ -0,0 +1,27 @@
+const resultContainer = document.querySelector(".result-container");
+export function createHtml(data) {
+  resultContainer.innerHTML = "";
+
+  for (let i = 0; i < 7; i++) {
+    resultContainer.innerHTML += `<div class="result">
+                                        <h4>${data[i].name}</h4>
+                                        <p>Birthday: <b>${
+                                          data[i].birthday
+                                        }</b></p>
+                                        <p>Nickname: <b>${
+                                          data[i].nickname
+                                        }</b></p>
+                                        <div>Occupations:                                         
+                                            ${createTags(data[i].occupation)}
+                                        </div>
+                                    </div>`;
+  }
+}
+
+export function createTags(list = []) {
+  let tags = "";
+
+  list.forEach((item) => (tags += `<span>${item}</span>`));
+
+  return tags;
+}
